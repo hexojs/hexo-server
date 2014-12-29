c2e0673538026b7c7c18f68c10ff4c585f0e7c9c
Tommy Chen <tommy351@gmail.com>
2014-12-29 17:14:02 +0800

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Server
+# hexo-server
 
-Server module for Hexo.
+[![Build Status](https://travis-ci.org/hexojs/hexo-server.svg?branch=master)](https://travis-ci.org/hexojs/hexo-server)  [![NPM version](https://badge.fury.io/js/hexo-server.svg)](http://badge.fury.io/js/hexo-server) [![Coverage Status](https://img.shields.io/coveralls/hexojs/hexo-server.svg)](https://coveralls.io/r/hexojs/hexo-server?branch=master)
+
+Server module for [Hexo].
+
+## Installation
+
+``` bash
+$ npm install hexo-server --save
+```
+
+## License
+
+MIT
+
+[Hexo]: http://hexo.io/
---
+++ index.js
@@ -0,0 +1,16 @@
+hexo.extend.filter.register('server_middleware', require('./lib/middlewares/logger'));
+hexo.extend.filter.register('server_middleware', require('./lib/middlewares/header'));
+hexo.extend.filter.register('server_middleware', require('./lib/middlewares/route'));
+hexo.extend.filter.register('server_middleware', require('./lib/middlewares/static'));
+hexo.extend.filter.register('server_middleware', require('./lib/middlewares/redirect'));
+hexo.extend.filter.register('server_middleware', require('./lib/middlewares/gzip'));
+
+hexo.extend.console.register('server', 'Start the server.', {
+  desc: 'Start the server and watch for file changes.',
+  options: [
+    {name: '-i, --ip', desc: 'Override the default server IP. Bind to all IP address by default.'},
+    {name: '-p, --port', desc: 'Override the default port.'},
+    {name: '-s, --static', desc: 'Only serve static files.'},
+    {name: '-l, --log [format]', desc: 'Enable logger. Override the logger format.'}
+  ]
+}, require('./lib/server'));
